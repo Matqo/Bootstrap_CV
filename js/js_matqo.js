775d8be315bfea9798a878ba3766e0734522ebf4
Matqo <martinfutas@gmail.com>
2016-12-07 01:52:52 +0000

    V0.9 = Experience and Portfolio sections added & magnific popups with filtering

--- js/js_matqo.js
+++ js/js_matqo.js
@@ -61,6 +61,53 @@
             }
         });
 
+        /* For Bootstrap current state on portfolio sorting */
+
+        $('ul.nav-pills li a').click(function(e) {
+            $('ul.nav-pills li.active').removeClass('active')
+            $(this).parent('li').addClass('active')
+        })
+
+        /* portfolio mixitup */
+
+        $(window).on('load', function() {
+            var $container = $('.grid-wrapper');
+            $container.isotope({
+                filter: '*',
+                animationOptions: {
+                    duration: 750,
+                    easing: 'linear',
+                    queue: false
+                }
+            });
+
+            $('.grid-controls li a').click(function() {
+                $('.grid-controls .current').removeClass('current');
+                $(this).addClass('current');
+
+                var selector = $(this).attr('data-filter');
+                $container.isotope({
+                    filter: selector,
+                    animationOptions: {
+                        duration: 750,
+                        easing: 'linear',
+                        queue: false
+                    }
+                });
+                return false;
+            });
+        });
+
+
+        /* Magnific Popup */
+        $('.grid-wrapper').magnificPopup({
+            delegate: 'a',
+            type: 'image',
+            gallery: {
+                enabled: true
+            }
+        });
+
 
         /*
                     $.srSmoothscroll({
